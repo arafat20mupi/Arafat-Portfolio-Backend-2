7c47c256b21355fea24c2cd43956af8c3bb5fa19
unknown <arafat.innotimes.net@gmail.com>
2025-10-05 23:41:50 +0600

    Project Complete

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.json()); 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://dev-arafat-islam.vercel.app",
     credentials: true,
   })
 );
